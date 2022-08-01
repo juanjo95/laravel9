a06e8992d51b46e243a4a830e037d679f43f741d
Juan J Salgado <zallagox95@hotmail.com>
2022-08-01 12:47:53 -0500

    feat 🚀: Routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * Route::get    Consultar
+ * Route::post   Guardar
+ * Route::put    Actualizar
+ * Route::delete Eliminar
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ruta home';
+});
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+Route::get('blog/{slug}', function ($slug) {
+    //Consulta a base de datos
+    return $slug;
+});
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
