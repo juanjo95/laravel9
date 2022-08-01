2e83405e18e7856909db432ee2cd42f32a52ceee
Juan J Salgado <zallagox95@hotmail.com>
2022-08-01 13:27:42 -0500

    feat 🚀: Templates

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
+@extends('template')
+
+@section('content')
     <h1>Listado</h1>
 
     @foreach ($posts as $post)
         <p>
             <strong>{{$post['id']}}</strong>
-            <a href="#">{{$post['title']}}</a>
+            <a href="{{ route('post', $post['slug']) }}">{{$post['title']}}</a>
         </p>
     @endforeach
-
-</body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 Route::get('blog', function () {
     //Consulta a base de datos
     $posts = [
@@ -13,10 +13,10 @@ Route::get('blog', function () {
     ];
 
     return view('blog', ['posts' => $posts]);
-});
+})->name('blog');
 Route::get('blog/{slug}', function ($slug) {
     //Consulta a base de datos
     $post = $slug;
 
     return view('post', ['post' => $post]);
-});
+})->name('post');
