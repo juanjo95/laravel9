943e85ce6693dedc975780009b7a926d264afd65
Juan J Salgado <zallagox95@hotmail.com>
2022-08-02 06:19:25 -0500

    feat 🚀: Listado publicaciones

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     public function index(){
-        return view('posts.index');
+        $posts = Post::latest()->paginate();
+        return view('posts.index', ['posts' => $posts]);
     }
 }
