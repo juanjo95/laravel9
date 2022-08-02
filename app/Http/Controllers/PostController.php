3cb85ec52ec851dd29d4f476c009523193720158
Juan J Salgado <zallagox95@hotmail.com>
2022-08-02 07:26:32 -0500

    feat 🚀: Funcion de guardar registro

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 class PostController extends Controller
 {
     public function index(){
@@ -13,6 +15,17 @@ class PostController extends Controller
 
     public function create(Post $post){
         return view('posts.create', ['post' => $post]);
+    }
+
+    public function store(Request $request){
+        /*  */
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug'  => Str::slug($title),
+            'body'  => $request->body,
+        ]);
+
+        return redirect()->route('posts.edit', $post);
     }
 
     public function edit(Post $post){
