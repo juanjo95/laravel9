40058fa0f03e1ddeb0d8d093caffd3f984f6507c
Juan J Salgado <zallagox95@hotmail.com>
2022-08-03 17:19:19 -0500

    feat 🚀: Diseño personalizado - Tailwind

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,7 +9,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="container px-4 mx-auto">
+    <div class="w-10/12 mx-auto">
         <header class="flex justify-between items-center py-4">
             <div class="flex items-center flex-grow gap-4">
                 <a href="{{ route('home') }}">
@@ -26,7 +26,15 @@
             @endauth
         </header>
 
+        <div class="opacity-60 h-px mb-8" style="background: linear-gradient(to right, rgba(200,200,200,0) 0%,rgba(200,200,200,1) 30%,rgba(200,200,200,1) 70%,rgba(200,200,200,0) 100%);">
+
+        </div>
+
         @yield('content')
+
+        <p class="py-16">
+            <img src="{{ asset('images/logo.png') }}" class="h-12 mx-auto">
+        </p>
     </div>
 </body>
 </html>
