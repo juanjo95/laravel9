1693350946753858e97cd001f11a0ea2a7720e3c
Juan J Salgado <zallagox95@hotmail.com>
2022-08-03 16:01:41 -0500

    feat 🚀: Diseño web

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,20 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proyecto web</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
-
-        @auth
-            <a href="{{ route('dashboard') }}">Dashboard</a>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo.png') }}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" name="" placeholder="Buscar">
+                </form>
+            </div>
+            @auth
+                <a href="{{ route('dashboard') }}">Dashboard</a>
             @else
-            <a href="{{ route('login') }}">Login</a>
-        @endauth
+                <a href="{{ route('login') }}">Login</a>
+            @endauth
+        </header>
 
-    </p>
-    <hr>
-    @yield('content')
+        @yield('content')
+    </div>
 </body>
 </html>
